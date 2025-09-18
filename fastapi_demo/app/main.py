f86763681c8670d58de6e89c012a2b573c54b804
hjtm130 <osechi130uto@gmail.com>
2025-09-18 15:58:05 +0900

    First project but its not work

--- fastapi_demo/app/main.py
+++ fastapi_demo/app/main.py
@@ -1,12 +1,5 @@
 from fastapi import FastAPI
+from app.api import forecast
 
 app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/api/forecast/{grid_id}")
-async def get_forecast(grid_id: int):
-    # ダミーデータ返すだけ
-    return {"grid_id": grid_id, "forecast": "Sunny"}
+app.include_router(forecast.router)
