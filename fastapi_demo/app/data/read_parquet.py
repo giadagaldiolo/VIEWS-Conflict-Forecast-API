faa0f7901bd4a710bed298a89378254fd55c1202
hjtm130 <osechi130uto@gmail.com>
2025-09-20 08:53:47 +0900

    changed read_parquet.py to read schema

--- fastapi_demo/app/data/read_parquet.py
+++ fastapi_demo/app/data/read_parquet.py
@@ -1,17 +1,8 @@
+import pyarrow.parquet as pq
 import pandas as pd
 
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 1000)  
+schema_001 = pq.read_schema('preds_001.parquet')
+print(schema_001)
 
-df = pd.read_parquet('preds_001.parquet', engine='pyarrow')
-print(df.head())
-
-df = pd.read_parquet('preds_001_90_hdi.parquet', engine='pyarrow')
-print(df.head())
-
-df = pd.read_csv('fatalities002_2025_07_t01_pgm.csv', engine='pyarrow')
-print(df.head())
-
-df = pd.read_csv('fatalities002_2025_07_t01_cm.csv', engine='pyarrow')
-print(df.head())
+schema_001_90_hdi = pq.read_schema('preds_001_90_hdi.parquet')
+print(schema_001_90_hdi)
