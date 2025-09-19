5675a9a5931adf11ef9aec1c9b55e7b23cfa7e03
hjtm130 <osechi130uto@gmail.com>
2025-09-20 04:37:01 +0900

    changed read_parquet.py to display all of column of head

--- fastapi_demo/app/data/read_parquet.py
+++ fastapi_demo/app/data/read_parquet.py
@@ -1,5 +1,9 @@
 import pandas as pd
 
+pd.set_option('display.max_colwidth', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)  
+
 df = pd.read_parquet('preds_001.parquet', engine='pyarrow')
 
 print(df.head())
