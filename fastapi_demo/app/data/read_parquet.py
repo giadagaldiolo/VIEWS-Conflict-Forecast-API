15f01958fc7ced167960e778bbd37d525db74eaa
hjtm130 <osechi130uto@gmail.com>
2025-09-20 07:24:42 +0900

    add read parquet & csv

--- fastapi_demo/app/data/read_parquet.py
+++ fastapi_demo/app/data/read_parquet.py
@@ -5,9 +5,13 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 1000)  
 
 df = pd.read_parquet('preds_001.parquet', engine='pyarrow')
-
 print(df.head())
 
 df = pd.read_parquet('preds_001_90_hdi.parquet', engine='pyarrow')
+print(df.head())
 
+df = pd.read_csv('fatalities002_2025_07_t01_pgm.csv', engine='pyarrow')
+print(df.head())
+
+df = pd.read_csv('fatalities002_2025_07_t01_cm.csv', engine='pyarrow')
 print(df.head())
