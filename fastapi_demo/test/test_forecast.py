b4fda7496c74df2fb49b95488dfe8ca3c48ef052
giadagaldiolo <giada.galdiolo@student.supsi.ch>
2025-09-20 11:11:26 +0300

    pytests work

--- fastapi_demo/test/test_forecast.py
+++ fastapi_demo/test/test_forecast.py
@@ -4,7 +4,7 @@ import json
 from fastapi.testclient import TestClient
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from main import app
 
 client = TestClient(app)
@@ -84,7 +84,7 @@ def test_forecasts_single_cell_all_metrics():
 def test_forecasts_multiple_cells_and_months_all_metrics():
     params = {
         "month_id": [409, 410],
-        "priogrid_id": [62356, 62357],
+        "priogrid_id": [62356, 81761],
         "metrics": ALL_METRICS
     }
     response = client.get("/api/preds_001/pgm/sb/forecasts", params=params)
