21b01c59601326eab9e99adbd7c05d5bdbe005a8
hjtm130 <osechi130uto@gmail.com>
2025-09-21 03:15:31 +0900

    delete files

--- fastapi_demo/frontend/src/components/types.ts
+++
@@ -1,9 +0,0 @@
-// types.ts
-export interface ForecastData {
-  priogrid_id: string;
-  country_id: string;
-  month_id: string;
-  lat: number;
-  lon: number;
-  metrics: Record<string, number | null>;
-}
