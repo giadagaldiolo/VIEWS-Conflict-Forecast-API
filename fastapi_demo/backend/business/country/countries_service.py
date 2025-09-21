297884776100098c4c2d855e184edf52e846e153
hjtm130 <osechi130uto@gmail.com>
2025-09-21 14:30:10 +0900

    add docstring to backend/business/country

--- fastapi_demo/backend/business/country/countries_service.py
+++ fastapi_demo/backend/business/country/countries_service.py
@@ -3,8 +3,27 @@ from dataAccess.interface_parquet_reader import IParquetReader
 from business.country.interface_country_service import ICountryService
 
 class CountryService(ICountryService):
+    """
+    Service class to handle operations related to countries.
+
+    Attributes:
+        repository (IParquetReader): Repository interface to read Parquet data.
+    """
+
     def __init__(self, repository: IParquetReader):
+        """
+        Initialize CountryService with a repository.
+
+        Args:
+            repository (IParquetReader): Instance implementing Parquet data reading.
+        """
         self.repository = repository
 
     def get_countries(self) -> List[int]:
+        """
+        Retrieve a list of country IDs from the repository.
+
+        Returns:
+            List[int]: List of country IDs.
+        """
         return self.repository.list_country_ids()
