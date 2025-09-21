76eb09cb0ecb796a6714ab9af72dee07ba516d5c
hjtm130 <osechi130uto@gmail.com>
2025-09-21 14:24:20 +0900

    add docstling to backend/business/cell

--- fastapi_demo/backend/business/cell/interface_cell_service.py
+++ fastapi_demo/backend/business/cell/interface_cell_service.py
@@ -2,6 +2,18 @@ from typing import List, Dict, Any, Optional
 from abc import ABC, abstractmethod
 
 class ICellService(ABC):
+    """
+    Interface for cell-related services.
+
+    Defines methods that any cell service implementation must provide.
+    """
+
     @abstractmethod
     def get_cells(self) -> List[int]:
+        """
+        Retrieve a list of priogrid IDs.
+
+        Returns:
+            List[int]: List of priogrid IDs.
+        """
         pass
